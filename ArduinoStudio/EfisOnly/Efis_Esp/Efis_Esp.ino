82c3da78f24b81164de485f42994f5acefcc05a0
Chris George <chris@chrisgeorge.org.uk>
2023-10-18 21:46:34 +0100

    refactored slightly and got the display layout almost right for the middle and right hand displays

--- ArduinoStudio/EfisOnly/Efis_Esp/Efis_Esp.ino
+++ ArduinoStudio/EfisOnly/Efis_Esp/Efis_Esp.ino
@@ -4,8 +4,11 @@
 //#include <Adafruit_SSD1306.h>
 #include <Adafruit_SH110X.h>
 #include <Fonts/FreeSans9pt7b.h>
+#include "Fonts/FreeSans6pt7b.h"
 #include "Fonts/DSEG7Classic_Regular20pt7b.h"  //https://github.com/keshikan/DSEG and https://rop.nl/truetype2gfx/
 #include "Fonts/DSEG7Classic_Regular22pt7b.h"
+#include "Fonts/DSEG7Classic_Italic14pt7b.h"
+#include "Fonts/DSEG14Classic_Italic14pt7b.h"
 
 #define I2C_MOBIFLIGHT_ADDR 0x27
 #define I2C_MOBIFLIGHT_SDA 21
@@ -23,8 +26,8 @@ TwoWire I2Ctwo = TwoWire(1);
 
 // address of the multiplexer to change the channels
 #define TCA9548A_I2C_ADDRESS  0x70
-#define TCA9548A_CHANNEL_EFIS_LEFT  0
-#define TCA9548A_CHANNEL_EFIS_RIGHT 1
+#define TCA9548A_CHANNEL_EFIS_LEFT  1
+#define TCA9548A_CHANNEL_EFIS_RIGHT 0
 
 Adafruit_SH1106G display(SCREEN_WIDTH, SCREEN_HEIGHT, &I2Ctwo, OLED_RESET);
 
@@ -32,7 +35,12 @@ bool isStd = false;
 bool isHpa = true;
 String valHpa="8888";
 String valHg="88,88";
-
+String RightBlock = "8888";
+int RightBlockMode = 0;
+bool BaroMode = 0;
+bool BaroDisplay = 0;
+bool AltArm = false;
+bool displayAll = true;
 
 void setup() {
   Serial.begin(115200);
@@ -73,14 +81,68 @@ void setup() {
 }
 
 void loop() {
-  updateDisplayRight();
-  updateDisplayLeft();
+  if (displayAll)
+  {
+    showAllRight();
+    showAllLeft();
+  } else {   
+    updateDisplayRight();
+    updateDisplayLeft();
+  }
   delay(50);
 
   //  delay(3000);
   //  Serial.println("Loop");
 }
 
+void showAllRight(void)
+{
+  setTCAChannel(TCA9548A_CHANNEL_EFIS_RIGHT);
+  display.clearDisplay();
+  display.setTextColor(SH110X_WHITE);        // Draw white text
+
+  display.setFont(&FreeSans6pt7b);
+  display.setTextSize(1);     
+  
+  display_alert();
+  display_ft();
+  display_hpa();
+  display_inhg();
+  display_fpm();
+  display_rightblock("12345");
+
+  display.display();
+
+//   delay(3000);
+//   display.clearDisplay();
+//   display_rightblock("4321");
+  
+//   display.display();
+
+//  delay(3000);
+//   display.clearDisplay();
+//   display_rightblock("789");
+  
+//   display.display();
+  
+//  delay(3000);
+}
+
+void showAllLeft(void)
+{ 
+  setTCAChannel(TCA9548A_CHANNEL_EFIS_LEFT);
+  display.clearDisplay();
+  display.setTextColor(SH110X_WHITE);        // Draw white text
+
+  display.setFont(&FreeSans6pt7b);
+  display.setTextSize(1);     
+
+  display_VS();
+  display_ALT();
+  display_ALTARM();
+  
+  display.display();
+}
 /*
   switch multiplexer channel
 */
@@ -129,100 +191,194 @@ void onReceive(int len){
 
 void handleCommand(String command){
     
-    // Serial.println("handleCommand");
-    // Serial.println(command);
-
-   if(command.startsWith("#0")){
-    valHpa=command.substring(2);
-    if(valHpa.substring(0,1)=="5"){
-      valHpa="0"+ valHpa.substring(1);
+  if(command.startsWith("#0")){
+       if(command.substring(2)=="0"){
+        RightBlockMode=0;
+      } else if (command.substring(2)=="1") {
+        RightBlockMode=1;
+      } else if (command.substring(2)=="2") {
+        RightBlockMode=2;
+      }
+  }
+  else if(command.startsWith("#1")){
+    Serial.println(command);
+     RightBlock=command.substring(2);
+  }
+  else if(command.startsWith("#2")){
+      if(command.substring(2)=="0"){
+        BaroMode=false;
+      }else{
+        BaroMode = true;
+      }
+  }
+  else if(command.startsWith("#3")){
+    if(command.substring(2)=="0"){
+      BaroDisplay=false;
+    }else{
+      BaroDisplay=true;
     }
-   }
-   if(command.startsWith("#1")){
-    valHg=command.substring(2);
-   }
-   if(command.startsWith("#2")){
-     if(command.substring(2)=="0"){
-      isHpa=false;
-     }else{
-       isHpa=true;
-     }
-   }
-   if(command.startsWith("#3")){
-     if(command.substring(2)=="1"){
-      isStd=true;
-     }else{
-       isStd=false;
-     }
-   } 
+  }
+  else if(command.startsWith("#4")){
+      if(command.substring(2)=="0"){
+        AltArm=false;
+      }else{
+        AltArm=true;
+      }
+  }
 }
+
 
 void updateDisplayRight(void)
 {
-
   setTCAChannel(TCA9548A_CHANNEL_EFIS_RIGHT);
 
  // Clear the buffer
   display.clearDisplay();
   display.setTextColor(SH110X_WHITE);        // Draw white text
 
-  if(isStd){
-      display.setFont(&DSEG7Classic_Regular22pt7b);
-      display.setCursor(20,60);             
-      display.println("5td");
-  }else{
-    display.setFont(&FreeSans9pt7b);
-    display.setTextSize(1);             
-    display.setCursor(75,15);             
-    display.println("RIGHT");
-    if(isHpa){
-        display.setFont(&DSEG7Classic_Regular20pt7b);
-        if(valHpa.length()==3)
-        {
-          display.setCursor(32,60);             
-        }else{
-          display.setCursor(0,60);     
-        }
-        display.println(valHpa);
-    }else {
-        display.setFont(&DSEG7Classic_Regular20pt7b);
-        display.setCursor(0,60);             
-        display.println(valHg); 
-    }
+  display.setFont(&FreeSans6pt7b);
+  display.setTextSize(1);     
+  // if(isAlert)
+  // {
+  //   displayAlert();
+  // }
+
+  if(BaroDisplay && BaroMode)
+  {
+    display_inhg();
   }
+
+  if(BaroDisplay && !BaroMode)
+  {
+    display_hpa();
+  }
+
+  // if(isFPM)
+  // { 
+  //   display_fpm();
+  // }
+
+  if(!BaroDisplay)
+  {
+    display_ft();
+  }
+
+  display_rightblock(RightBlock);
+
+  // display.setFont(&DSEG7Classic_Italic14pt7b); //&DSEG7Classic_Regular9pt7b);
+  // display.setCursor(58,30);             
+  // display.println("500");
+  
+  // display.setFont(&DSEG14Classic_Italic14pt7b); //&DSEG7Classic_Regular9pt7b);
+  // display.setCursor(40,38);             
+  // display.println(",");
 
   // Show the display buffer on the screen. You MUST call display() after
   // drawing commands to make them visible on screen!
   display.display();
-
 }
+
 
 void updateDisplayLeft(void)
 {
-
   setTCAChannel(TCA9548A_CHANNEL_EFIS_LEFT);
   
  // Clear the buffer
   display.clearDisplay();
   display.setTextColor(SH110X_WHITE);        // Draw white text
 
-  if(isStd){
-      display.setFont(&DSEG7Classic_Regular22pt7b);
-      display.setCursor(20,60);             
-      display.println("5td");
-  }else{
-    display.setFont(&FreeSans9pt7b);
-    display.setTextSize(1);             
-    display.setCursor(75,15);             
-    display.println("LEFT");
-
-    display.setFont(&DSEG7Classic_Regular20pt7b);
-    display.setCursor(0,60);             
-    display.println(valHg); 
-  }
-
+  display_VS();
+  display_ALT();
+  display_ALTARM();
+ 
   // Show the display buffer on the screen. You MUST call display() after
   // drawing commands to make them visible on screen!
   display.display();
 
+}
+
+
+// Right hand section
+
+void display_inhg(void)
+{
+  display.setCursor(80,61);             
+  display.println("IN  HG");
+}
+
+void display_hpa(void)
+{
+  display.setCursor(25,61);             
+  display.println("HPA");
+}
+
+void display_ft(void)
+{
+  display.setCursor(102,47);             
+  display.println("FT");
+}
+
+void display_fpm(void)
+{
+  display.setCursor(66,47);             
+  display.println("FPM");
+}
+
+void display_alert(void)
+{
+  display.setCursor(10,47);             
+  display.println("ALERT");
+}
+
+void display_rightblock(String rightBlockValue)
+{
+  String rightString = rightBlockValue.substring(rightBlockValue.length()-3);
+  String leftString = rightBlockValue.substring(0,rightBlockValue.length()-3);
+
+  display.setFont(&DSEG7Classic_Italic14pt7b); //&DSEG7Classic_Regular9pt7b);
+  display.setCursor(55,30);             
+  display.println(rightString);
+    
+  if (leftString.length() > 0) {
+    display.setFont(&DSEG7Classic_Italic14pt7b); //&DSEG7Classic_Regular9pt7b);
+    if (leftString.length()==2){  
+      display.setCursor(4,30); 
+    } else if (leftString.length()==1) {
+      display.setCursor(25,30); 
+    }            
+    display.println(leftString);
+
+    display.setFont(&DSEG14Classic_Italic14pt7b); //&DSEG7Classic_Regular9pt7b);
+    display.setCursor(43,38);             
+    display.println(",");
+  }
+}
+
+
+// Middle section
+
+void display_VS(void)
+{
+  display.setFont(&DSEG14Classic_Italic14pt7b);
+  display.setCursor(45,25);             
+  display.println("VS");
+}
+
+void display_ALT(void)
+{
+  
+  display.setFont(&DSEG14Classic_Italic14pt7b);
+  display.setCursor(20,61);             
+  display.println("ALT");
+}
+
+void display_ALTARM(void)
+{
+  display.setFont(&FreeSans6pt7b);
+  display.setCursor(90,36);
+  display.println("A");
+  display.setCursor(90,48);
+  display.println("R");
+  display.setCursor(90,60);
+  display.println("M");
 }
