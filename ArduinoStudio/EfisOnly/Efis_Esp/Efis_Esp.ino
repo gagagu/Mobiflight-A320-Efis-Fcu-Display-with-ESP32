14030724ca4755567d1e98491e3d9b779b95a73b
Chris George <chris@chrisgeorge.org.uk>
2023-10-20 17:54:58 +0100

    Added support for left and centre data display

--- ArduinoStudio/EfisOnly/Efis_Esp/Efis_Esp.ino
+++ ArduinoStudio/EfisOnly/Efis_Esp/Efis_Esp.ino
@@ -9,10 +9,11 @@
 #include "Fonts/DSEG7Classic_Regular22pt7b.h"
 #include "Fonts/DSEG7Classic_Italic14pt7b.h"
 #include "Fonts/DSEG14Classic_Italic14pt7b.h"
+#include "Fonts/DSEG14Classic_Regular14pt7b.h"
 
 #define I2C_MOBIFLIGHT_ADDR 0x27
-#define I2C_MOBIFLIGHT_SDA 22
-#define I2C_MOBIFLIGHT_SCL 21
+#define I2C_MOBIFLIGHT_SDA 21
+#define I2C_MOBIFLIGHT_SCL 22
 
 #define I2C_DISPLAY_SDA 17
 #define I2C_DISPLAY_SCL 16
@@ -26,9 +27,9 @@ TwoWire I2Ctwo = TwoWire(1);
 
 // address of the multiplexer to change the channels
 #define TCA9548A_I2C_ADDRESS  0x70
-#define TCA9548A_CHANNEL_EFIS_LEFT  2
-#define TCA9548A_CHANNEL_EFIS_CENTRE  1
-#define TCA9548A_CHANNEL_EFIS_RIGHT 0
+#define TCA9548A_CHANNEL_EFIS_LEFT  0
+#define TCA9548A_CHANNEL_EFIS_CENTRE 1
+#define TCA9548A_CHANNEL_EFIS_RIGHT 2
 
 Adafruit_SH1106G display(SCREEN_WIDTH, SCREEN_HEIGHT, &I2Ctwo, OLED_RESET);
 
@@ -41,6 +42,16 @@ int RightBlockMode = 0;
 bool BaroMode = 0;
 bool BaroDisplay = 0;
 bool AltArm = false;
+bool NavArm = false;
+bool AprArm = false;
+bool RevArm = false;
+bool HdgLock = false;
+bool VertHold = false;
+bool AltHold = false;
+bool Nav1Lock = false;
+bool ROL_WingLeveler = false;
+bool ROL_REV = false;
+bool ROL_APR = false;
 bool displayAll = true;
 
 void setup() {
@@ -52,8 +63,8 @@ void setup() {
   Wire.begin((uint8_t)I2C_MOBIFLIGHT_ADDR,I2C_MOBIFLIGHT_SDA,I2C_MOBIFLIGHT_SCL,400000);
   I2Ctwo.begin(I2C_DISPLAY_SDA,I2C_DISPLAY_SCL,400000); // SDA pin 16, SCL pin 17, 400kHz frequency
 
-
   setTCAChannel(TCA9548A_CHANNEL_EFIS_LEFT);
+
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SCREEN_ADDRESS, true)) {
@@ -65,19 +76,6 @@ void setup() {
   // the library initializes this with an Adafruit splash screen.
   display.display();
   updateDisplayLeft();
-
-  setTCAChannel(TCA9548A_CHANNEL_EFIS_CENTRE);
-
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SCREEN_ADDRESS, true)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  updateDisplayCentre();
 
   setTCAChannel(TCA9548A_CHANNEL_EFIS_RIGHT);
 
@@ -91,6 +89,19 @@ void setup() {
   // the library initializes this with an Adafruit splash screen.
   display.display();
   updateDisplayRight();
+
+  setTCAChannel(TCA9548A_CHANNEL_EFIS_CENTRE);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SCREEN_ADDRESS, true)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  updateDisplayCentre();
 
 }
 
@@ -144,6 +155,7 @@ void showAllRight(void)
 //  delay(3000);
 }
 
+
 void showAllCentre(void)
 { 
   setTCAChannel(TCA9548A_CHANNEL_EFIS_CENTRE);
@@ -153,23 +165,30 @@ void showAllCentre(void)
   display.setFont(&FreeSans6pt7b);
   display.setTextSize(1);     
 
-  display_VS();
+  //display_VS();
   display_ALT();
   display_ALTARM();
   
+  display_PitchTrimUP();
+  display_PitchTrimText();
+  display_PitchTrimDOWN();
+
   display.display();
 }
+
 
 void showAllLeft(void)
 { 
   setTCAChannel(TCA9548A_CHANNEL_EFIS_LEFT);
-   display.clearDisplay();
+  display.clearDisplay();
   display.setTextColor(SH110X_WHITE);        // Draw white text
 
-  display_ROLLMODE_APR();
-  display_AP_Symbol();
-  display_NAV();
+  display.setFont(&FreeSans6pt7b);
+  display.setTextSize(1);     
+
+  display_ROLLMODE_HDG();  
   display_ALTARM();
+  display_AP_Symbol();
   
   display.display();
 }
@@ -186,10 +205,12 @@ void setTCAChannel(byte i){
 void onReceive(int len){
   // Serial.println("OnReceive");
   
+  displayAll = false;
+
   char msgArray[9]="";
-  // Serial.println(Wire.available());
+  //Serial.println(Wire.available());
   // // if smaller than 32 ignore
-   if(Wire.available()==32)
+   if(Wire.available()>=32)
    {
      for (int i=0; i <= 8; i++){
        uint8_t hibits = (uint8_t)Wire.read();      
@@ -211,6 +232,7 @@ void onReceive(int len){
     handleCommand(String(msgArray));
 
    }else{
+    //Serial.println("Not 32");
     while(Wire.available()){
       Wire.read();
     }
@@ -220,19 +242,19 @@ void onReceive(int len){
 
 
 void handleCommand(String command){
-    
+  
   if(command.startsWith("#0")){
        if(command.substring(2)=="0"){
         RightBlockMode=0;
       } else if (command.substring(2)=="1") {
-        RightBlockMode=1;
+        RightBlockMode=1;        
       } else if (command.substring(2)=="2") {
         RightBlockMode=2;
       }
   }
   else if(command.startsWith("#1")){
-    Serial.println(command);
-     RightBlock=command.substring(2);
+    int rightBlockLen = command.substring(2,3).toInt();
+    RightBlock=command.substring(8-rightBlockLen);
   }
   else if(command.startsWith("#2")){
       if(command.substring(2)=="0"){
@@ -249,11 +271,67 @@ void handleCommand(String command){
     }
   }
   else if(command.startsWith("#4")){
-      if(command.substring(2)=="0"){
-        AltArm=false;
-      }else{
-        AltArm=true;
-      }
+    HdgLock=false;
+    Nav1Lock=false;
+    ROL_WingLeveler=false;
+    ROL_REV=false;
+    ROL_APR=false;
+
+    if(command.charAt(2)=='1'){
+      HdgLock=true;
+    }
+
+    if(command.charAt(3)=='1'){        
+      Nav1Lock=true;
+    }
+
+    if(command.charAt(4)=='1'){
+      ROL_WingLeveler=true;
+    }
+
+    if(command.charAt(5)=='1'){
+      ROL_REV=true;
+    }
+
+    if(command.charAt(6)=='1'){
+      ROL_APR=true;
+    }
+  }
+  else if(command.startsWith("#5")){
+    VertHold=false;
+    AltHold=false;
+
+    if(command.charAt(2)=='1'){
+      VertHold=true;
+    }
+    if(command.charAt(3)=='1'){
+      AltHold=true;
+    }
+  }
+  else if(command.startsWith("#6")){
+     
+  }
+  else if(command.startsWith("#7")){
+    AltArm=false;
+    NavArm=false;
+    AprArm=false;
+    RevArm=false;
+
+    if(command.charAt(2)=='1'){
+      AltArm=true;
+    }
+    if(command.charAt(3)=='1'){
+      NavArm=true;
+    }
+    if(command.charAt(4)=='1'){
+      AprArm=true;
+    }
+    if(command.charAt(5)=='1'){
+      RevArm=true;
+    }
+  }
+  else if(command.startsWith("#8")){
+     
   }
 }
 
@@ -282,26 +360,17 @@ void updateDisplayRight(void)
   {
     display_hpa();
   }
-
-  // if(isFPM)
-  // { 
-  //   display_fpm();
-  // }
-
-  if(!BaroDisplay)
+  
+  if(RightBlockMode == 0)
   {
     display_ft();
   }
-
-  display_rightblock(RightBlock);
-
-  // display.setFont(&DSEG7Classic_Italic14pt7b); //&DSEG7Classic_Regular9pt7b);
-  // display.setCursor(58,30);             
-  // display.println("500");
+  else if(RightBlockMode == 1) 
+  {
+    display_fpm();
+  }
   
-  // display.setFont(&DSEG14Classic_Italic14pt7b); //&DSEG7Classic_Regular9pt7b);
-  // display.setCursor(40,38);             
-  // display.println(",");
+  display_rightblock(RightBlock);
 
   // Show the display buffer on the screen. You MUST call display() after
   // drawing commands to make them visible on screen!
@@ -317,9 +386,17 @@ void updateDisplayCentre(void)
   display.clearDisplay();
   display.setTextColor(SH110X_WHITE);        // Draw white text
 
-  display_VS();
-  display_ALT();
-  display_ALTARM();
+  if (VertHold) {
+    display_VS();
+  }
+
+  if (AltHold) {
+    display_ALT();
+  }
+
+  if (AltArm) {
+    display_ALTARM();
+  }
  
   // Show the display buffer on the screen. You MUST call display() after
   // drawing commands to make them visible on screen!
@@ -330,6 +407,44 @@ void updateDisplayCentre(void)
 void updateDisplayLeft(void)
 {
   setTCAChannel(TCA9548A_CHANNEL_EFIS_LEFT);
+  display.clearDisplay();
+  display.setTextColor(SH110X_WHITE);        // Draw white text
+
+  if(HdgLock){
+    display_ROLLMODE_HDG();
+  }
+  
+  if(ROL_WingLeveler){
+    display_ROLLMODE_ROL();
+  }
+
+  if(Nav1Lock){
+    display_ROLLMODE_NAV();
+  }
+
+  if(ROL_APR){
+    display_ROLLMODE_APR();
+  }
+
+  if(ROL_REV){
+    display_ROLLMODE_REV();
+  }
+
+  if(NavArm) {
+    display_NAVARM();    
+  }
+
+  if(AprArm) {
+    display_APRARM();
+  }
+
+  if(RevArm) {
+    display_REVARM();
+  }
+
+  if(HdgLock || ROL_WingLeveler || Nav1Lock || ROL_APR || ROL_REV) {
+    display_AP_Symbol();
+  }
 
   display.display();
 }
@@ -394,6 +509,12 @@ void display_rightblock(String rightBlockValue)
 
 // Middle section
 
+void display_ALT(void)
+{
+  display.setFont(&DSEG14Classic_Italic14pt7b);
+  display.setCursor(20,25);             
+  display.println("ALT");
+}
 void display_VS(void)
 {
   display.setFont(&DSEG14Classic_Italic14pt7b);
@@ -401,15 +522,17 @@ void display_VS(void)
   display.println("VS");
 }
 
-void display_ALT(void)
+void display_ALTARM(void)
 {
   
-  display.setFont(&DSEG14Classic_Italic14pt7b);
+  display.setFont(&DSEG14Classic_Regular14pt7b);
   display.setCursor(20,61);             
   display.println("ALT");
+
+  display_MIDARM();
 }
 
-void display_ALTARM(void)
+void display_MIDARM(void)
 {
   display.setFont(&FreeSans6pt7b);
   display.setCursor(90,36);
@@ -420,14 +543,39 @@ void display_ALTARM(void)
   display.println("M");
 }
 
+void display_PitchTrimUP(void)
+{
+  display.fillTriangle(115,15,123,15,119,4,SH110X_WHITE);
+}
 
-// Middle section
+void display_PitchTrimText(void)
+{
+  display.setFont(&FreeSans6pt7b);
+  display.setCursor(110,30);
+  display.println("P");
+  display.setCursor(110,42);
+  display.println("T");
+}
+
+void display_PitchTrimDOWN(void)
+{
+  display.fillTriangle(115,47,123,47,119,58,SH110X_WHITE);
+}
+
+// Left section
 
 void display_ROLLMODE_ROL(void)
 {
   display.setFont(&DSEG14Classic_Italic14pt7b);
   display.setCursor(20,25);             
   display.println("ROL");
+}
+
+void display_ROLLMODE_NAV(void)
+{
+  display.setFont(&DSEG14Classic_Italic14pt7b);
+  display.setCursor(20,25);             
+  display.println("NAV");
 }
 
 void display_ROLLMODE_HDG(void)
@@ -469,18 +617,31 @@ void display_AP_Symbol(void)
   display.drawFastVLine(125, 4, 13, SH110X_WHITE);
 }
 
-void display_NAV(void)
+void display_NAVARM(void)
 {
-  display.setFont(&DSEG14Classic_Italic14pt7b);
+  display.setFont(&DSEG14Classic_Regular14pt7b);
   display.setCursor(20,61);             
   display.println("NAV");
+
+  display_LEFTARM();
 }
-void display_REV(void)
+void display_REVARM(void)
 {
-  display.setFont(&DSEG14Classic_Italic14pt7b);
+  display.setFont(&DSEG14Classic_Regular14pt7b);
   display.setCursor(20,61);             
   display.println("REV");
+
+  display_LEFTARM();
 }
+void display_APRARM(void)
+{
+  display.setFont(&DSEG14Classic_Regular14pt7b);
+  display.setCursor(20,61);             
+  display.println("APR");
+
+  display_LEFTARM();
+}
+
 void display_GS(void)
 {
   display.setFont(&DSEG14Classic_Italic14pt7b);
